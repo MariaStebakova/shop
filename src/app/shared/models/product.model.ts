4be587cfc6031d023cc22b6cbd0d6286bb5ffc78
VZhyrytskiy <Vitaliy_Zhyrytskyy@epam.com>
2023-08-08 16:22:21 +0300

    task1-comm

--- src/app/shared/models/product.model.ts
+++ src/app/shared/models/product.model.ts
@@ -1,17 +1,12 @@
-import { Category } from "src/app/shared/enums/category.enum";
-
+import { Category } from 'src/app/shared/enums/category.enum';
+// Так меньше кода
 export class ProductModel {
-    name: string;
-    description: string;
-    price: number;
-    category: Category;
-    isAvailable: boolean;
-
-    constructor(name: string, description: string, price: number, category: Category, isAvailable: boolean) {
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.category = category;
-        this.isAvailable = isAvailable;
-    }
+  constructor(
+    public name: string,
+    public description: string,
+    public price: number,
+    public category: Category,
+    public isAvailable: boolean
+  ) {
+  }
 }
